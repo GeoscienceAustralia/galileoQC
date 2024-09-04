Better grid masking, minor bug fixes
1. Incorporate masking of grids by distance from samples.
2. Allow 'um/s/s' as gravity units in gravity correction checks.
3. check for 'NRG' linetype in updateLineAttributes()

AirGravQC/gridFiles/diff_n_image.py
@@ -38,7 +38,7 @@ import AirGravQC.gridFiles.gridutility as gut
 
 # GRID REPORTING
 
-def diff_n_image(whizz_file, channel1, channel2, grid_space, method='neighbours', mask_polygon=[], numneighbours=1):
+def diff_n_image(whizz_file, channel1, channel2, grid_space, *, method='neighbours', mask_polygon=[], mask_pixels=0, numneighbours=1):
     """
     Subtracts the data in `channel2` from those in `channel1`, then grids and images that difference.
 
@@ -55,6 +55,11 @@ def diff_n_image(whizz_file, channel1, channel2, grid_space, method='neighbours'
     mask_polygon : numpy 2D array, optional
         If the size of mask_polygon > 0, then data_array will be masked to the area
         within the polygon defined by it.
+    mask_pixels : Integer, optional
+        If mask_pixels > 0, then all pixels further than `mask_pixels * grid_space` from a data
+        location will be masked out. Default 0.
+    numneighbours : Integer, optional
+        If method='neighbours', then this is the number of neighbours to average. Default 5.
 
     Returns
     -------
@@ -68,7 +73,8 @@ def diff_n_image(whizz_file, channel1, channel2, grid_space, method='neighbours'
     my_data1.attrs['title'] = f'{channel1} - {channel2}'
     if 'units' in my_data2[channel2].attrs:
         my_data1[channel1].attrs['units'] =  my_data2[channel2].attrs['units']
-    my_grid, my_region = xarray_to_grid(my_data1, grid_space, method=method, mask_polygon=mask_polygon, numneighbours=numneighbours)
+    my_grid, my_region = xarray_to_grid(my_data1, grid_space, method=method, mask_polygon=mask_polygon, 
+        mask_pixels=mask_pixels, numneighbours=numneighbours)
 
     xdImage(my_grid, my_grid.attrs['title'], colormap=cc.m_CET_L9, cmap_norm='nonorm', 
         minClip=np.nan, maxClip=np.nan, gridlines=True, cb_ticks='stats', nSigma=2,

AirGravQC/gridFiles/oddeven.py
@@ -204,7 +204,8 @@ def calcMeanTrack(lineGroup, easting, northing):
     return np.mean(track[idx1:idx2])
 
 
-def oddevenlines(whizz_file, channel, grid_space, oddlines=[], evenlines=[], method='neighbours', mask_polygon=[], numneighbours=1):
+def oddevenlines(whizz_file, channel, grid_space, oddlines=[], evenlines=[], method='neighbours', 
+    mask_polygon=[], mask_pixels=0, numneighbours=1):
     """
     Performs odd-even analysis of the `channel` data in `whizz_file`. The data are
     sorted into two sets of odd and even lines. Each set is gridded and the difference
@@ -228,6 +229,11 @@ def oddevenlines(whizz_file, channel, grid_space, oddlines=[], evenlines=[], met
     mask_polygon : numpy 2D array, optional
         If the size of mask_polygon > 0, then data_array will be masked to the area
         within the polygon defined by it.
+    mask_pixels : Integer, optional
+        If mask_pixels > 0, then all pixels further than `mask_pixels * grid_space` from a data
+        location will be masked out. Default 0.
+    numneighbours : Integer, optional
+        If method='neighbours', then this is the number of neighbours to average. Default 5.
 
     Returns
     -------
@@ -270,9 +276,9 @@ def oddevenlines(whizz_file, channel, grid_space, oddlines=[], evenlines=[], met
                         
     # Grid and difference the data sets
     even_grid, even_region = xarray_to_grid(even_data, grid_space, region=intersectregion, method=method, 
-        mask_polygon=mask_polygon, numneighbours=numneighbours)
+        mask_polygon=mask_polygon, mask_pixels=mask_pixels, numneighbours=numneighbours)
     odd_grid, odd_region = xarray_to_grid(odd_data, grid_space, region=intersectregion, method=method, 
-        mask_polygon=mask_polygon, numneighbours=numneighbours)
+        mask_polygon=mask_polygon, mask_pixels=mask_pixels, numneighbours=numneighbours)
     d_grid = even_grid - odd_grid
 
     # Subtraction does not preserve attributes

AirGravQC/gridFiles/xarray_to_grid.py
@@ -18,7 +18,7 @@ groupName = config.groupName
 projectName = config.projectName
 
 
-def xarray_to_grid(my_data, grid_space, region=[], method='scipy', mask_polygon=[], numneighbours=5):
+def xarray_to_grid(my_data, grid_space, region=[], method='neighbours', mask_polygon=[], mask_pixels=0, numneighbours=5):
     """
     Interpolates `my_data` onto a regular grid.
 
@@ -34,10 +34,13 @@ def xarray_to_grid(my_data, grid_space, region=[], method='scipy', mask_polygon=
     method : string, optional
         The gridding algorithm to use in interpolating the data. Available is the Verde nearest
         neighbour method - "neighbours" and the SciPy GridData "linear" method. "neighbours" is
-        much faster if `pykdtree` is installed. Default SciPy `linear` method.
+        much faster if `pykdtree` is installed. Default `neighbours` method.
     mask_polygon : numpy 2D array, optional
         If the size of mask_polygon > 0, then data_array will be masked to the area
         within the polygon defined by it.
+    mask_pixels : Integer, optional
+        If mask_pixels > 0, then all pixels further than `mask_pixels * grid_space` from a data
+        location will be masked out. Default 0.
     numneighbours : Integer, optional
         If method='neighbours', then this is the number of neighbours to average. Default 5.
 
@@ -103,9 +106,9 @@ def xarray_to_grid(my_data, grid_space, region=[], method='scipy', mask_polygon=
         tmpgrid = xr.DataArray(np.zeros((len(X), len(Y))), coords=[X, Y], dims=['x', 'y'])
         grid = tmpgrid.transpose()
 
-        X, Y = np.meshgrid(X, Y)  # 2D grid for interpolation
+        east, north = np.meshgrid(X, Y)  # 2D grid for interpolation
 
-        grid.values = griddata(list(zip(my_data[x_chan].data, my_data[y_chan].data)), my_data[z_chan].data, (X, Y), method='linear')
+        grid.values = griddata(list(zip(my_data[x_chan].data, my_data[y_chan].data)), my_data[z_chan].data, (east, north), method='linear')
 
     elif method == 'neighbours':
         if region == []:
@@ -127,6 +130,15 @@ def xarray_to_grid(my_data, grid_space, region=[], method='scipy', mask_polygon=
 
     if np.array(mask_polygon).size > 0:
         grid = gut.maskGridByPolygon(grid, mask_polygon, x_chan='x', y_chan='y')
+
+    if mask_pixels > 0:
+        mymask = vd.distance_mask(
+            (my_data[x_chan], my_data[y_chan]),
+            maxdist = grid_space * mask_pixels,
+            coordinates = (east, north),
+        )
+        grid = grid.where(mymask)
+
 
     grid.attrs['units'] = myunits
     grid.attrs['long_name'] = z_chan