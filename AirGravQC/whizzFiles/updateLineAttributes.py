055a4fe40d2fb76bf75900b6cbd80e13787e53d3
Mark Dransfield <114627462+bamburgh@users.noreply.github.com>
2025-05-08 17:21:58 +1000

    updateLineAttributes() can now check a list of plan-files for the planned line number attribute.

--- AirGravQC/whizzFiles/updateLineAttributes.py
+++ AirGravQC/whizzFiles/updateLineAttributes.py
@@ -5,6 +5,7 @@ Update the metadata for a `line group` in a `geoWhizz` file.
 """
 import numpy as np
 import h5py
+import pathlib
 
 import AirGravQC.config as config
 import AirGravQC.utility.utility as util
@@ -12,14 +13,14 @@ import AirGravQC.utility.utility as util
 groupName = config.groupName
 
 
-def updateLineAttributes(whizzFile, planfile='', line_type='', line='', planned_line=0, flight_chan='', date_chan='', verbose=False):
+def updateLineAttributes(whizzFile, planfiles=None, line_type='', line='', planned_line=0, flight_chan='', date_chan='', verbose=False):
     """
     For each line group, use the line_type field to set the associated planned
     line number, line variety, segment number, and reflight number for the line.
 
     If a planfile is given, then the line and planned_line parameters are ignored
     and the planned, segment and reflight attributes are set for all lines in
-    whizzfile that also exist in planfile.
+    whizzfile that also exist in planfiles.
 
     If no planfile is given, and both line and planned_line have non-default values,
     then the planned attribute of line is set to planned_line. No segment or reflight
@@ -32,12 +33,12 @@ def updateLineAttributes(whizzFile, planfile='', line_type='', line='', planned_
     ----------
     whizzFile : String or pathlib Path
         Name of a HDF5 Whizz observations file, including path and extension.
-    planfile : String or pathlib Path, optional
-        Name of a HDF5 Whizz plan file, including path and extension. Default is ''
+    planfiles : Either String or pathlib Path of list of same, optional
+        Name of HDF5 Whizz plan file(s), including path and extension. Default is None
         which results in no planned_line attributes being written to the observations
         whizz file.
     line_type : TYPE, optional
-        Either 'Xcal_nsw' or 'SGL_GA' or 'SGL_NSW' or 'NRG' or 'Xcal_can'.
+        Either 'Xcal_nsw' or 'SGL_GA' or 'SGL_NSW' or SGL_GDF' or 'NRG' or 'Xcal_can'.
         The default is '' which causes the `line`s 'PlannedLine' attribute to be set
         to `planned_line`.
     line : String
@@ -68,11 +69,21 @@ def updateLineAttributes(whizzFile, planfile='', line_type='', line='', planned_
         _updateDateAttributes(whizzFile, date_chan)
 
     filename = str(whizzFile)
-    if planfile == '':
+    if planfiles is None:
         no_plan = True
-    else:
+        print('NO ACTION TAKEN ON LINE_TYPE - no plan file provided.')
+
+    elif type(planfiles) is list:
+        if not (type(planfiles[0]) is str or type(planfiles[0]) is pathlib.PosixPath):
+            print("ERROR - the input parameter needs to be a list of planfile names but it is not.")
+            return
         no_plan = False
-        planfile_str = str(planfile)
+    elif type(planfiles) is pathlib.PosixPath or type(planfiles) is str:
+        planfiles = [planfiles]
+        no_plan = False
+    else:
+        print("ERROR - the input parameter needs to be a list of planfile names but it is not.")
+        return
 
     with h5py.File(filename, 'r+') as f:
         # all the lines are in g:
@@ -81,114 +92,127 @@ def updateLineAttributes(whizzFile, planfile='', line_type='', line='', planned_
         if line != '' and planned_line != 0:
             gg = g[line]
             gg.attrs['PlannedLine'] = planned_line
-            print('One planned line updated.')
-        elif no_plan:
-            print('NO ACTION TAKEN ON LINE_TYPE - no plan file provided.')
-        elif line_type in ['Xcal_nsw', 'Xcal_can', 'SGL_GA', 'SGL_NSW', 'NRG', 'SGL_GDF']:
-            print(f'\nSetting Line attributes for {whizzFile.name} according to the {line_type} scheme.')
-            print(f'Verifying planned line numbers against {planfile_str}.')
+            print(f'PlannedLine attribute of line {line} set to {planned_line}.')
+            return
+        if not line_type in ['Xcal_nsw', 'Xcal_can', 'SGL_GA', 'SGL_NSW', 'NRG', 'SGL_GDF']:
+            print(f"NO ACTION TAKEN ON LINE_TYPE - line_type {line_type} not in ['Xcal_nsw', 'Xcal_can', 'SGL_GA', 'SGL_GDF', 'SGL_NSW', 'NRG'].")
+            return
 
-            with h5py.File(planfile_str, 'r') as pf:
-                pfg = pf[groupName]['Lines']
-                if verbose:
-                    print('  {:<14} {:<14} {:<14} {:<14} '.format('Line No.','Plan Line No.', 'Segment No.', 'Re-flight No.'))
-                for line in g:
-                    gg = g[line]
-                    current_line = gg.attrs['LineNumber']
-                    gg.attrs['LineType'] = line_type
-                    if line_type == 'Xcal_nsw':
-                        if current_line < 8999999.0:
-                            gg_planned_line = np.floor(current_line / 100.0) * 10.0
-                            gg.attrs['Segment'] = 0
-                            gg.attrs['ReflightNumber'] = int(current_line - np.floor(current_line / 10.0) * 10)
-                        else:
-                            gg_planned_line = np.floor(current_line / 10000.0)
-                            gg.attrs['Segment'] = 0
-                            gg.attrs['ReflightNumber'] = int(current_line - np.floor(current_line / 10000.0) * 10000)
-                        seconddigit = str(current_line)[1]
-                        if seconddigit == '9':
-                            gg.attrs['LineVariety'] = "Control"
-                        else:
-                            gg.attrs['LineVariety'] = "Traverse"
-                    elif line_type == 'Xcal_can':
-                        if current_line < 8999999.0:
-                            gg_planned_line = np.floor(current_line / 10.0) * 10.0
-                            gg.attrs['Segment'] = 0
-                            gg.attrs['ReflightNumber'] = int(current_line - np.floor(current_line / 10.0) * 10)
-                        else:
-                            gg_planned_line = np.floor(current_line / 10000.0)
-                            gg.attrs['Segment'] = 0
-                            gg.attrs['ReflightNumber'] = int(current_line - np.floor(current_line / 10000.0) * 10000)
-                        seconddigit = str(current_line)[1]
-                        if seconddigit == '9':
-                            gg.attrs['LineVariety'] = "Control"
-                        else:
-                            gg.attrs['LineVariety'] = "Traverse"
-                    elif line_type == 'NRG':
-                        gg_planned_line = np.floor(current_line / 10.0) * 10.0
-                        gg.attrs['Segment'] = 0
-                        gg.attrs['ReflightNumber'] = int(current_line - np.floor(current_line / 10.0) * 10)
-                        if current_line > 89999:
-                            gg.attrs['LineVariety'] = "Control"
-                        else:
-                            gg.attrs['LineVariety'] = "Traverse"
-                    elif line_type == 'SGL_GA':
-                        if current_line < 7000:
-                            gg_planned_line = np.floor(current_line * 10.0) / 10.0
-                            current_segment = int(np.round(10 * (current_line - np.floor(current_line))))
-                            gg.attrs['Segment'] = current_segment
-                            gg.attrs['ReflightNumber'] = int(np.round(100 * (current_line - np.floor(current_line)))
-                                                             - 10 * current_segment)
-                        else:
-                            gg_planned_line = np.floor(current_line)
-                            gg.attrs['Segment'] = 0
-                            gg.attrs['ReflightNumber'] = int(100 * (current_line - np.floor(current_line)))
-                        if current_line < 999.9:
-                            gg.attrs['LineVariety'] = "Control"
-                        else:
-                            gg.attrs['LineVariety'] = "Traverse"
-                    elif line_type == 'SGL_NSW':
-                        if current_line > 1000000:
-                            gg_planned_line = np.floor(current_line / 100.0) / 10.0
-                        else:
-                            gg_planned_line = np.floor(current_line / 100.0) / 10.0
-                        if current_line < 999.9:
-                            gg.attrs['LineVariety'] = "Control"
-                        else:
-                            gg.attrs['LineVariety'] = "Traverse"
-                    elif line_type == 'SGL_GDF':
-                        if current_line < 7000:
-                            gg_planned_line = np.floor(current_line / 1000.0)
-                            current_segment = int(np.round(10 * (current_line - np.floor(current_line))))
-                            gg.attrs['Segment'] = current_segment
-                            gg.attrs['ReflightNumber'] = int(np.round(100 * (current_line - np.floor(current_line)))
-                                                             - 10 * current_segment)
-                        else:
-                            gg_planned_line = np.floor(current_line / 1000.0)
-                            gg.attrs['Segment'] = 0
-                            gg.attrs['ReflightNumber'] = int(100 * (current_line - np.floor(current_line)))
-                        if current_line < 999999.9:
-                            gg.attrs['LineVariety'] = "Control"
-                        else:
-                            gg.attrs['LineVariety'] = "Traverse"
+        print(f'\nSetting Line attributes for {whizzFile.name} according to the {line_type} scheme.')
+        if not no_plan:
+            print(f'Verifying planned line numbers against provided plan file(s).')
 
-                    # check that gg_planned_line is in plan_file
-                    lineInGroup, _ = util.getLineNumberInGroup(pfg, gg_planned_line)
-                    if lineInGroup:
-                        gg.attrs['PlannedLine'] = gg_planned_line
-                    else:
-                        print(f'Calculated planned line {gg_planned_line} not found in {planfile_str}.')
-                    if verbose:
-                        print('  {:<14} {:<14} {:<14} {:<14} '.\
-                            format(current_line, gg.attrs['PlannedLine'], gg.attrs['Segment'], gg.attrs['ReflightNumber']))
-                if verbose:
-                    print('\n')
-        else:
-            print(f"NO ACTION TAKEN ON LINE_TYPE - line_type {line_type} not in ['Xcal_nsw', 'Xcal_can', 'SGL_GA', 'SGL_NSW', 'NRG'].")
+        report = ''
+        if verbose:
+            print('  {:<14} {:<14} {:<14} {:<14} '.format('Line No.','Plan Line No.', 'Segment No.', 'Re-flight No.'))
+        for line in g:
+            gg = g[line]
+            current_line = gg.attrs['LineNumber']
+            gg.attrs['LineType'] = line_type
+            plan_line, segment_line, reflight_line, variety_line = _decode_linenumber(current_line, line_type)
+            gg.attrs['Segment'] = segment_line
+            gg.attrs['ReflightNumber'] = reflight_line
+            gg.attrs['LineVariety'] = variety_line
+
+            # check that plan_line is in plan_file
+            if not no_plan:
+                for planfile in planfiles:
+                    planfile_str = str(planfile)
+                    with h5py.File(planfile_str, 'r') as pf:
+                        pfg = pf[groupName]['Lines']
+                        lineInGroup = False
+                        lineInGroup, _ = util.getLineNumberInGroup(pfg, plan_line)
+                        if lineInGroup:
+                            gg.attrs['PlannedLine'] = plan_line
+                            break
+                if not lineInGroup:
+                    report += f'Line {current_line} - calculated planned line {plan_line} not found in any provided plan file.'
+            if verbose:
+                if no_plan or not lineInGroup:
+                    print('  {:<14} {:<14} {:<14} {:<14} '.\
+                        format(current_line, ' ', gg.attrs['Segment'], gg.attrs['ReflightNumber']))
+                else:
+                    print('  {:<14} {:<14} {:<14} {:<14} '.\
+                        format(current_line, gg.attrs['PlannedLine'], gg.attrs['Segment'], gg.attrs['ReflightNumber']))
+        if verbose:
+            print('\n')
 
     return
 
 
+
+def _decode_linenumber(current_line, line_type):
+    if line_type == 'Xcal_nsw':
+        if current_line < 8999999.0:
+            plan_line = np.floor(current_line / 100.0) * 10.0
+            segment_line = 0
+            reflight_line = int(current_line - np.floor(current_line / 10.0) * 10)
+        else:
+            plan_line = np.floor(current_line / 10000.0)
+            segment_line = 0
+            reflight_line = int(current_line - np.floor(current_line / 10000.0) * 10000)
+        seconddigit = str(current_line)[1]
+        if seconddigit == '9':
+            variety_line = "Control"
+        else:
+            variety_line = "Traverse"
+    elif line_type == 'Xcal_can':
+        if current_line < 8999999.0:
+            plan_line = np.floor(current_line / 10.0) * 10.0
+            segment_line = 0
+            reflight_line = int(current_line - np.floor(current_line / 10.0) * 10)
+        else:
+            plan_line = np.floor(current_line / 10000.0)
+            segment_line = 0
+            reflight_line = int(current_line - np.floor(current_line / 10000.0) * 10000)
+        seconddigit = str(current_line)[1]
+        if seconddigit == '9':
+            variety_line = "Control"
+        else:
+            variety_line = "Traverse"
+    elif line_type == 'NRG':
+        plan_line = np.floor(current_line / 10.0) * 10.0
+        segment_line = 0
+        reflight_line = int(current_line - np.floor(current_line / 10.0) * 10)
+        if current_line > 89999:
+            variety_line = "Control"
+        else:
+            variety_line = "Traverse"
+    elif line_type == 'SGL_GA':
+        if current_line < 7000:
+            plan_line = np.floor(current_line * 10.0) / 10.0
+            current_segment = int(np.round(10 * (current_line - np.floor(current_line))))
+            segment_line = current_segment
+            reflight_line = int(np.round(100 * (current_line - np.floor(current_line)))
+                                             - 10 * current_segment)
+        else:
+            plan_line = np.floor(current_line)
+            segment_line = 0
+            reflight_line = int(100 * (current_line - np.floor(current_line)))
+        if current_line < 999.9:
+            variety_line = "Control"
+        else:
+            variety_line = "Traverse"
+    elif line_type == 'SGL_NSW':
+        if current_line > 1000000:
+            plan_line = np.floor(current_line / 100.0) / 10.0
+        else:
+            plan_line = np.floor(current_line / 100.0) / 10.0
+        if current_line < 999.9:
+            variety_line = "Control"
+        else:
+            variety_line = "Traverse"
+    elif line_type == 'SGL_GDF':
+        plan_line = np.floor(current_line / 1000.0)
+        remaining = (current_line - plan_line * 1000.0)
+        segment_line = np.floor(remaining / 100.0)
+        reflight_line = int((remaining - 100.0 * segment_line))
+        if current_line < 999999.9:
+            variety_line = "Control"
+        else:
+            variety_line = "Traverse"
+
+    return plan_line, segment_line, reflight_line, variety_line
 
 def _updateFlightAttributes(whizzFile, flight_chan):
     filename = str(whizzFile)
