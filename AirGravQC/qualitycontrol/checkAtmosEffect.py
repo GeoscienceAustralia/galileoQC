a3f68a7b4271d2710a704c30af50db0e06c4fc65
Mark Dransfield <114627462+bamburgh@users.noreply.github.com>
2025-05-26 11:20:56 +1000

    Improve checkAtmosEffect docstring

--- AirGravQC/qualitycontrol/checkAtmosEffect.py
+++ AirGravQC/qualitycontrol/checkAtmosEffect.py
@@ -29,7 +29,8 @@ def checkAtmosEffect(whizzFile, atmosCorr, lines=[], GRS80_height=''):
     whizzFile : String or pathlib Path
         Name of a HDF5 Whizz file, including path and extension.
     atmosCorr : String
-        The name of the geoWhizz field or channel containing the atmospheric correction.
+        The name of the geoWhizz field or channel containing the atmospheric correction. These
+        data must be in units of 'gu', 'µm/s/s', 'um/s/s' or 'um/s2'.
     lines : Array{String}, optional
         Array of line numbers as strings. Default = [], meaning all lines are checked.
     GRS80_height : String, optional
