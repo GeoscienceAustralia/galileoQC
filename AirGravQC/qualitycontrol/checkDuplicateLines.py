e0c29bc82427a0478ea75f976bc3ad1452661be0
Mark Dransfield <114627462+bamburgh@users.noreply.github.com>
2025-08-23 14:47:58 +1000

    added checkDuplicateLines.py
    to catch duplication of line numbers in input data.

---
+++ AirGravQC/qualitycontrol/checkDuplicateLines.py
@@ -0,0 +1,31 @@
+def checkDuplicateLines(whizzFile):
+    """
+    Check the flight-lines in whizzfile and report any duplicate group names.
+
+    Parameters
+    ----------
+    whizzFile : String or pathlib Path
+        Name of a HDF5 Whizz file, including path and extension.
+
+    Returns
+    -------
+    None.
+
+    """
+    filename = str(whizzFile)
+
+    with h5py.File(filename, 'r+') as f:
+        # create all the data structure ready for the datasets
+        g = f[groupName]['Lines']
+        flightlines = list(g.keys())
+    # `set` removes non-unique elements. 
+    seen = set()
+
+    # Check for duplicates
+    unique = True
+    for x in flightlines:
+        if x in seen:
+            unique = False
+            print(f'WARNING - duplicate flight-line {x} in {filename}.')
+            break
+        seen.add(x)            
